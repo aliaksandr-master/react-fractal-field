d2586571073b05e74092565a40169d485361e689
alxe_master <alxe.master@gmail.com>
2017-12-20 14:37:09 +0300

    remove from exporting unimplemented api

--- example-2/__tests__/index.test.js
+++ example-2/__tests__/index.test.js
@@ -1,15 +1,15 @@
 /*eslint-env jest*/
 import '../index';
-import { triggerChange, triggerInitialize, triggerFocus, triggerBlur, triggerSubmit, triggerReset, onError } from '../../lib';
+import { triggerChange, /*triggerInitialize, triggerFocus, triggerBlur, */triggerSubmit, /*triggerReset, */onError } from '../../lib';
 
 test('example-2', () => {
   triggerChange('main_radio', 'some-1');
-  triggerInitialize('main_radio');
-  triggerFocus('main_radio');
-  triggerFocus('main_radio');
-  triggerBlur('main_radio');
+  //triggerInitialize('main_radio');
+  //triggerFocus('main_radio');
+  //triggerFocus('main_radio');
+  //triggerBlur('main_radio');
   triggerSubmit('main_radio');
-  triggerReset('main_radio');
+  //triggerReset('main_radio');
   onError(() => {});
   expect(true).toBe(true);
 });
